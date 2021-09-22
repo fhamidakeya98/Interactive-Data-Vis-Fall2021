9085c4e18ffb85fec51eeac88f8b13d7c079c227
Fhamida <fhamidakeya00@gmail.com>
2021-09-22 13:22:52 -0400

    Completed Tutorial 3

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+let button = document.getElementById("clickme"), count = 0;
+
+button.onclick = function() { 
+    count += 1;
+    displayBoard.innerHTML = "Clicker Count: " + count;
+};
+function userName() {
+    let x = 
+        document.getElementById("userName").value;
+    
+    document.getElementById(
+      "displayName").innerHTML = x;
+}
