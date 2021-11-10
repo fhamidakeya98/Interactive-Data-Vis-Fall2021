a59fd29aa71fc1c724bbeee309735755d2f69bde
Fhamida <fhamidakeya00@gmail.com>
2021-11-10 15:51:48 -0500

    Complete hw

--- 3_1_quantities_and_amounts/main.js
+++ 3_1_quantities_and_amounts/main.js
@@ -1,40 +1,82 @@
 /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ,
-//   radius = ;
-
-// // since we use our scales in multiple functions, they need global scope
-// let xScale, yScale;
-
-/* APPLICATION STATE */
-let state = {
-  // data: [],
-};
+const height = 500;
+const margin = 100;
+const width = window.innerWidth *0.6  ;
 
 /* LOAD DATA */
-d3.csv('[PATH_TO_YOUR_DATA]', d3.autoType).then(raw_data => {
-  console.log("data", raw_data);
-  // save our data to application state
-  state.data = raw_data;
-  init();
-});
+d3.csv('../data/exdata.csv', d3.autoType)
+.then(data => {
+  console.log("data", data)
 
-/* INITIALIZING FUNCTION */
-// this will be run *one time* when the data finishes loading in
-function init() {
-  /* SCALES */
+ /* SCALES */
+//x scale - Count 
+const xScale = d3.scaleLinear()
+              .domain([0, d3.max(data, d => d.Amount)])
+              .range([margin,width-margin])
+              
+
+// yscale Activity
+const yScale = d3.scaleBand()
+              .domain(data.map(d => d.Country))
+              .range([height-margin,margin])
+              .paddingInner([0.2])
+              .paddingOuter([0.1])
 
 
+//Color of the Bars
+const colorScale = d3.scaleOrdinal(d3.schemeAccent)
+              .domain(data.map(d => d.Country));
+              
 
-  draw(); // calls the draw function
-}
-
-/* DRAW FUNCTION */
-// we call this every time there is an update to the data/state
-function draw() {
   /* HTML ELEMENTS */
- 
+ // svg
+ const svg = d3.select("#container")
+            .append("svg")
+            .attr("width",width)
+            .attr("height",height)
 
+ // Bars
+        svg.selectAll("rect")
+          .data(data)
+          .join("rect")
+          .attr("width", d=> xScale(d.Amount))
+          .attr("height", yScale.bandwidth())
+          .attr("x", margin)
+          .attr("y", d=> yScale(d.Country))
+          .attr("fill", d=> colorScale(d.Country)) 
 
-}
+        svg.append("g")
+          .attr("class","x-axis")
+          .style("transform", `translate(0px,${height-margin}px)`)
+          .call(d3.axisBottom(xScale))
+          
+        svg.append("g")
+          .attr("class","y-axis")
+          .style("transform", `translate(${margin}px,0px)`)
+          .call(d3.axisLeft(yScale))
+
+//Title of the X and Y Axis
+
+        svg.append("text")
+          .attr("y", margin / 2 )
+          .attr("x", -margin * 3)
+          .attr("transform", "rotate(-90)")
+          .style("font-size", "1.3 rem")
+          .text("Country");
+
+          svg.append("text")
+          .attr("text-anchor", "end")
+          .attr("x", (width / 2.3) + margin)
+          .attr("y", height - margin * .3)
+          .style("font-size", "1.3 rem")
+          .text("Annual Fish Consumption (Tons)");
+
+          svg.append("text")
+          .attr("text-anchor", "end")
+          .attr("x", (width / 2.3) + margin)
+          .attr("y", height - margin * 4.1)
+          .style("font-weight", "bold")
+          .style("font-size", "1.3 rem")
+          .text("Countries with Most Fish Consumption");
+
+ })
